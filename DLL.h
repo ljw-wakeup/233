bb8c8d256ec4169362653d377d23deb7dca1e139
ljw-wakeup <33725889+ljw-wakeup@users.noreply.github.com>
2018-04-15 23:54:31 +0800

    Add files via upload
    233

---
+++ DLL.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<string>
+using namespace std;
+struct Answer {
+	string* consequnce;
+	string* express;
+};
+extern "C++" _declspec(dllexport) Answer set(int ExpNumber, int operator_number, int Accuracy, bool fraction, bool decimal, int min, int max, int operator_mode);
